9c41dde469f7a0a51498c86adbd62945fb51db28
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-07-21 00:47:21 +0300

    UNIT_SIZE

--- Kadabra/src/main.rs
+++ Kadabra/src/main.rs
@@ -1,12 +1,23 @@
 use macroquad::prelude::*;
 
 
-pub const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
+const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
+
+const UNIT_SIZE: Vec2 = const_vec2!([40., 60.]);
+
 
 #[macroquad::main("breakout")]
 async fn main() {
+    let unit_rect = Rect::new(
+        screen_width() * 0.5 - UNIT_SIZE.x * 0.5,
+        screen_height() * 0.5 - UNIT_SIZE.y * 0.5,
+        UNIT_SIZE.x,
+        UNIT_SIZE.y,
+    );
+
     loop {
         clear_background(GROUND_COLOR);
+        draw_rectangle(unit_rect.x, unit_rect.y, unit_rect.w, unit_rect.h, DARKGRAY);
         next_frame().await
     }
 }
