1a48f7d8d894c44023c2caac3f1cdf7f87c3b3f5
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-07-20 21:31:56 +0300

    GROUND_COLOR

--- Kadabra/src/main.rs
+++ Kadabra/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+
+pub const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
+
+#[macroquad::main("breakout")]
+async fn main() {
+    loop {
+        clear_background(GROUND_COLOR);
+        next_frame().await
+    }
 }
