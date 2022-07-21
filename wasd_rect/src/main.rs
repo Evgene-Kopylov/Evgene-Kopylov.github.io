5e69942b7bd3c8f2582b31067ddeb0ea0727457a
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-07-22 01:43:42 +0300

    refactor

---
+++ wasd_rect/src/main.rs
@@ -0,0 +1,82 @@
+use macroquad::prelude::*;
+
+
+const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
+const UNIT_COLOR: Color = DARKGRAY;
+const UNIT_SIZE: Vec2 = const_vec2!([40., 40.]);
+const UNIT_SPEED: f32 = 300.0;
+
+struct Unit {
+    rect: Rect
+}
+
+impl Unit {
+    pub fn new() -> Self {
+        Self {
+            rect: Rect::new(
+                screen_width() * 0.5 - UNIT_SIZE.x * 0.5,
+                screen_height() * 0.5 - UNIT_SIZE.y * 0.5,
+                UNIT_SIZE.x,
+                UNIT_SIZE.y,
+            )
+        }
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        let mut x_move = 0f32;
+        if is_key_down(KeyCode::Left) {
+            x_move -= 1f32;
+        }
+        if is_key_down(KeyCode::Right) {
+            x_move += 1f32;
+        }
+
+        let mut y_move = 0f32;
+        if is_key_down(KeyCode::Up) {
+            y_move -= 1f32;
+        }
+        if is_key_down(KeyCode::Down) {
+            y_move += 1f32;
+        }
+
+        if self.rect.x < 1f32 {
+            x_move = 1f32;
+        }
+        if self.rect.x > screen_width() - self.rect.w {
+            x_move = -1f32;
+        }
+
+        if self.rect.y < 1f32 {
+            y_move = 1f32;
+        }
+        if self.rect.y > screen_height() - self.rect.h {
+            y_move = -1f32;
+        }
+
+        self.rect.x += x_move * dt * UNIT_SPEED;
+        self.rect.y += y_move * dt * UNIT_SPEED;
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(
+            self.rect.x,
+            self.rect.y,
+            self.rect.w,
+            self.rect.h,
+            UNIT_COLOR
+        );
+    }
+}
+
+
+#[macroquad::main("breakout")]
+async fn main() {
+    let mut unit = Unit::new();
+
+    loop {
+        unit.update(get_frame_time());
+        clear_background(GROUND_COLOR);
+        unit.draw();
+        next_frame().await
+    }
+}
