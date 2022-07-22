1aa49bd7c736ba2b56fcea8be7e711a72362ad10
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-07-22 13:38:09 +0300

    круг вокруг юнита

--- Kadabra/src/main.rs
+++ Kadabra/src/main.rs
@@ -3,22 +3,22 @@ use macroquad::prelude::*;
 
 const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
 const UNIT_COLOR: Color = DARKGRAY;
-const UNIT_SIZE: Vec2 = const_vec2!([40., 40.]);
+const UNIT_SIZE: (f32, f32) = (50.0, 75.0);
 const UNIT_SPEED: f32 = 300.0;
 
 struct Unit {
-    rect: Rect
+    collision: Circle,
 }
 
 impl Unit {
     pub fn new() -> Self {
         Self {
-            rect: Rect::new(
-                screen_width() * 0.5 - UNIT_SIZE.x * 0.5,
-                screen_height() * 0.5 - UNIT_SIZE.y * 0.5,
-                UNIT_SIZE.x,
-                UNIT_SIZE.y,
+            collision: Circle::new(
+                screen_width() * 0.5 - UNIT_SIZE.1 * 0.5,
+                screen_height() * 0.5 - UNIT_SIZE.1 * 0.5,
+                UNIT_SIZE.1 / 2.
             )
+
         }
     }
 
@@ -39,31 +39,45 @@ impl Unit {
             y_move += 1f32;
         }
 
-        if self.rect.x < 1f32 {
+        if self.collision.x < 1f32 {
             x_move = 1f32;
         }
-        if self.rect.x > screen_width() - self.rect.w {
+        if self.collision.x > screen_width() - UNIT_SIZE.0 {
             x_move = -1f32;
         }
 
-        if self.rect.y < 1f32 {
+        if self.collision.y < 1f32 {
             y_move = 1f32;
         }
-        if self.rect.y > screen_height() - self.rect.h {
+        if self.collision.y > screen_height() - UNIT_SIZE.1 {
             y_move = -1f32;
         }
 
-        self.rect.x += x_move * dt * UNIT_SPEED;
-        self.rect.y += y_move * dt * UNIT_SPEED;
+        self.collision.x += x_move * dt * UNIT_SPEED;
+        self.collision.y += y_move * dt * UNIT_SPEED;
     }
 
-    pub fn draw(&self) {
-        draw_rectangle(
-            self.rect.x,
-            self.rect.y,
-            self.rect.w,
-            self.rect.h,
-            UNIT_COLOR
+    pub fn draw_collision(&self) {
+        draw_circle_lines(
+            self.collision.x + UNIT_SIZE.0 * 0.5,
+            self.collision.y + UNIT_SIZE.1 * 0.5,
+            self.collision.r,
+            1.,
+            RED
+        )
+    }
+
+    pub fn draw(&self, texture: Texture2D) {
+        draw_texture_ex(
+            texture,
+            self.collision.x,
+            self.collision.y,
+            UNIT_COLOR,
+            DrawTextureParams {
+                dest_size: Some(Vec2::new(UNIT_SIZE.0, UNIT_SIZE.1)),
+                rotation: 0.,
+                ..Default::default()
+            }
         );
     }
 }
@@ -71,27 +85,14 @@ impl Unit {
 
 #[macroquad::main("breakout")]
 async fn main() {
-    let mut unit = Unit::new();
     let texture: Texture2D = load_texture("materials/path726-5.png").await.unwrap();
+    let mut unit = Unit::new();
 
     loop {
         unit.update(get_frame_time());
         clear_background(GROUND_COLOR);
-        unit.draw();
-
-        draw_texture_ex(
-            texture,
-            // screen_width() / 2. - texture.width() / 2.,
-            // screen_height() / 2. - texture.height() / 2.,
-            0.,
-            1.,
-            GOLD,
-            DrawTextureParams {
-                dest_size: Some(Vec2::new(100.0, 100.0)),
-                rotation: -50.,
-                ..Default::default()
-            }
-        );
+        unit.draw_collision();
+        unit.draw(texture);
 
         next_frame().await
     }
