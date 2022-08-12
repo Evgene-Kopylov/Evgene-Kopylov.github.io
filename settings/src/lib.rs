9ffb41593244e9df0011106cb383f3a1cece350f
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-08-12 09:53:59 +0200

    cosmetic

--- settings/src/lib.rs
+++ settings/src/lib.rs
@@ -7,7 +7,6 @@ use macroquad::prelude::{
 
 
 pub const GROUND_COLOR: Color = Color::new(0.8, 0.8, 0.8, 1.00);
-// pub const UNIT_COLOR: Color = GRAY;
 pub const UNIT_COLOR: Color = ORANGE;
 pub const SELECTOR_COLOR: Color = YELLOW;
 pub const UNIT_SIZE: (f32, f32) = (60.0, 75.0);
