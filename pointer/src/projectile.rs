bd3554d5766a0d93ba4e4d60746ee19633c583ff
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-10-02 01:08:35 +0200

    duplication fix
    pointer

--- pointer/src/projectile.rs
+++ pointer/src/projectile.rs
@@ -30,8 +30,8 @@ impl Projectile {
     pub fn draw(&self) {
         draw_texture_ex(
             self.texture,
-            self.position.0 - self.size.0 * 0.5,
-            self.position.1 - self.size.1 * 0.5,
+            self.position.0 - self.size.0 * 0.50,
+            self.position.1 - self.size.1 * 0.50,
             PROJECTILE_COLOR,
             DrawTextureParams {
                 dest_size: Some(Vec2::new(self.size .0, self.size.1)),
